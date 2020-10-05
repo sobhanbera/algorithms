d6fd2f487a6a22f7324920b4a622cf40305f0b10
Barabás Ákos <akos.barabas2002@gmail.com>
2020-10-05 17:37:11 +0200

    MergeSort python

--- algorithms/python/MergeSort.py
+++ algorithms/python/MergeSort.py
@@ -1,35 +1,29 @@
-def merge_lists(left_sublist,right_sublist):
-	i,j = 0,0
-	result = []
-	#iterate through both left and right sublist
-	while i<len(left_sublist) and j<len(right_sublist):
-		#if left value is lower than right then append it to the result
-		if left_sublist[i] <= right_sublist[j]:
-			result.append(left_sublist[i])
-			i += 1
-		else:
-			#if right value is lower than left then append it to the result
-			result.append(right_sublist[j])
-			j += 1
-	#concatenate the rest of the left and right sublists
-	result += left_sublist[i:]
-	result += right_sublist[j:]
-	#return the result
-	return result
+def merge_lists(left, right):
+    i, j = 0, 0
+    result = []
 
-def merge_sort(input_list):
-	#if list contains only 1 element return it
-	if len(input_list) <= 1:
-		return input_list
-	else:
-		#split the lists into two sublists and recursively split sublists
-		midpoint = int(len(input_list)/2)
-		left_sublist = merge_sort(input_list[:midpoint])
-		right_sublist = merge_sort(input_list[midpoint:])
-		#return the merged list using the merge_list function above
-		return merge_lists(left_sublist,right_sublist)
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+		
+    result += left[i:]
+    result += right[j:]
+
+    return result
 
 
-#test run
-number_list = [3,1,5,3,2,5,8,2,9,6,12,53,75,22,83,123,12123]
-print merge_sort(number_list)
+def merge_sort(list):
+    if len(list) <= 1:
+        return list
+    else:
+        midpoint = int(len(list) / 2)
+        left = merge_sort(list[:midpoint])
+        right = merge_sort(list[midpoint:])
+        return merge_lists(left, right)
+
+number_list = [5, 12, 31, 7, 234, 7, 1, 9, 3, 4, 634, 1, 0, 65, 234, 9]
+print(merge_sort(number_list))
