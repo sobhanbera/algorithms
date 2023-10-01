a05d2ad2a418f05a6480a645be2d9a28bffa8dbd
zony <69179733+janak108@users.noreply.github.com>
2023-10-01 22:24:13 +0545

    Create Bogo sort.c
    implementation of bogo short in C

---
+++ algorithms/c/Bogo sort.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Function to check if the array is sorted
+int is_sorted(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+// Function to shuffle the array
+void shuffle(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        int j = rand() % n;
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+// Function to implement bogo short algorithm
+void bogo_short(int arr[], int n) {
+    while (!is_sorted(arr, n)) {
+        shuffle(arr, n);
+    }
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // Seed the random number generator with the current time
+    srand(time(0));
+
+    // Call the bogo short function
+    bogo_short(arr, n);
+
+    // Print the sorted array
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
