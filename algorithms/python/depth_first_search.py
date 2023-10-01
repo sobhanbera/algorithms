dc8a4efdef759a113dc55a5930b9b4ef0f3bb50b
shohan2001 <shohanmondal3@gmail.com>
2023-10-01 19:48:21 +0530

    adding comments and formatting the code

--- algorithms/python/depth_first_search.py
+++ algorithms/python/depth_first_search.py
@@ -1,31 +1,44 @@
-def dfs(graph, start):
-    visited = set()
-    stack = [start]
-    
-    while stack:
-        node = stack.pop()
-        if node not in visited:
-            visited.add(node)
-            print(node, end=' ') 
-            stack.extend(neighbor for neighbor in graph[node] if neighbor not in visited)
+# Define a recursive Depth-First Search (DFS) function to traverse a graph and print nodes in DFS order.
+def dfs(graph, node, visited):
+    # Mark the current node as visited.
+    visited.add(node)
+    # Print the current node (you can replace this with any desired action).
+    print(node, end=' ')
 
-            
-        
-    
+    # Recursively visit all unvisited neighbors of the current node.
+    for neighbor in graph[node]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
 
+# Define a function to add edges to the graph.
 def addEdges(graph, u, v):
     graph[u].append(v)
     graph[v].append(u)
 
-
+# Main function to input the graph and start DFS.
 def main():
+    # Input the number of nodes.
     n = int(input("Enter the number of nodes: "))
+    # Input the number of edges.
     m = int(input("Enter the number of edges: "))
+    
+    # Create an empty graph represented as an adjacency list.
     graph = [[] for node in range(n)]
+    
+    # Assuming nodes are numbered from 0 to n-1
+    # Input edges and add them to the graph.
     for edge in range(m):
         start, end = map(int, input().split())
         addEdges(graph, start, end)
-    dfs(graph, 0)
     
+    # Create a set to keep track of visited nodes.
+    visited = set()
+    
+    # Perform DFS from each unvisited node.
+    for node in range(n):
+        if node not in visited:
+            dfs(graph, node, visited)
+
+# Entry point of the program.
 if __name__ == '__main__':
-    main() 
+    main()
